116b1ce64f25d046f1e60c2cabd5d215d5524a69
Leonardo Molinaro <126402616+LMolinaro01@users.noreply.github.com>
2024-10-15 17:04:54 -0300

    Agendamento - Timer para Meses, Dias, Horas, min e segundos

--- python_cam.py
+++ python_cam.py
@@ -127,16 +127,14 @@ def capturar_fotos_continuamente(intervalo):
         if image_data:
             image = Image.open(io.BytesIO(image_data))
             salvar_automatico(image)
-        time.sleep(intervalo)  # Intervalo entre cada foto
+        time.sleep(intervalo) 
 
 def salvar_automatico(image):
     global pasta_salvar
-    # Gera o nome do arquivo baseado na data e hora
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     file_name = f"foto_{timestamp}.jpg"
     file_path = os.path.join(pasta_salvar, file_name)
 
-    # Adiciona data e hora na imagem e salva automaticamente
     adicionar_data_hora(image)
     image.save(file_path)
     print(f"Imagem salva automaticamente em: {file_path}")
@@ -147,7 +145,6 @@ def abrir_janela_temporizador():
     janela_temporizador.geometry("400x300")
     janela_temporizador.title("Captura com Temporizador")
 
-    # Label e Entry para intervalo do temporizador
     temporizador_label = ctk.CTkLabel(janela_temporizador, text="Intervalo do Temporizador:") 
     temporizador_label.pack(pady=5)
 
@@ -155,42 +152,36 @@ def abrir_janela_temporizador():
     temporizador_intervalo_entry = ctk.CTkEntry(janela_temporizador)
     temporizador_intervalo_entry.pack(pady=5)
 
-    # ComboBox para selecionar a unidade de tempo
     global unidade_temporizador
     unidade_temporizador = ctk.CTkComboBox(janela_temporizador, values=["Segundos", "Minutos", "Horas"])
-    unidade_temporizador.set("Segundos")  # Padrão é "Segundos"
+    unidade_temporizador.set("Segundos") 
     unidade_temporizador.pack(pady=5)
 
-    # Botão para iniciar captura de imagens
     btn_iniciar_captura = ctk.CTkButton(janela_temporizador, text="Iniciar Captura", command=iniciar_captura_temporizador)
     btn_iniciar_captura.pack(padx=50, pady=10, fill='x')
 
-    # Botão para parar captura de imagens
     btn_parar_captura = ctk.CTkButton(janela_temporizador, text="Parar Captura", command=parar_captura_temporizador)
     btn_parar_captura.pack(padx=50, pady=10, fill='x')
 
-    # Botão para voltar para a janela principal
     btn_voltar = ctk.CTkButton(janela_temporizador, text="Voltar", command=janela_temporizador.destroy)
     btn_voltar.pack(pady=15)
 
 def iniciar_captura_temporizador():
     global captura_ativa
     captura_ativa = True
-    escolher_pasta_salvar()  # Escolhe a pasta antes de iniciar a captura
+    escolher_pasta_salvar()
     if not pasta_salvar:
-        return  # Cancela a operação se o usuário não escolher a pasta
+        return 
 
-    # Desabilitar o ComboBox durante a captura
     unidade_temporizador.configure(state='disabled')
 
-    # Converte o intervalo para segundos
     intervalo = int(temporizador_intervalo_entry.get())
     unidade = unidade_temporizador.get()
 
     if unidade == "Minutos":
-        intervalo *= 60  # Converte minutos para segundos
+        intervalo *= 60  
     elif unidade == "Horas":
-        intervalo *= 3600  # Converte horas para segundos
+        intervalo *= 3600 
 
     threading.Thread(target=capturar_fotos_continuamente, args=(intervalo,)).start()
 
@@ -199,7 +190,6 @@ def parar_captura_temporizador():
     captura_ativa = False
     messagebox.showinfo("Captura Parada", "A captura de fotos foi parada.")
 
-    # Reabilitar o ComboBox ao parar a captura
     unidade_temporizador.configure(state='normal')
 
 def escolher_pasta_salvar():
@@ -211,17 +201,16 @@ def escolher_pasta_salvar():
 def abrir_janela_agendar_captura():
     global janela_agendar
     janela_agendar = ctk.CTkToplevel(app)
-    janela_agendar.geometry("400x300")
+    janela_agendar.geometry("400x350")
     janela_agendar.title("Agendar Captura")
 
-    # Label e Entry para data e hora
     data_label = ctk.CTkLabel(janela_agendar, text="Data (DD/MM/AAAA):") 
     data_label.pack(pady=5)
 
     global data_entry
     data_entry = ctk.CTkEntry(janela_agendar)
     data_entry.pack(pady=5)
-    data_entry.bind("<KeyRelease>", formatar_data)  # Formatação automática de data
+    data_entry.bind("<KeyRelease>", formatar_data) 
 
     hora_label = ctk.CTkLabel(janela_agendar, text="Hora (HH:MM):") 
     hora_label.pack(pady=5)
@@ -229,57 +218,49 @@ def abrir_janela_agendar_captura():
     global hora_entry
     hora_entry = ctk.CTkEntry(janela_agendar)
     hora_entry.pack(pady=5)
-    hora_entry.bind("<KeyRelease>", formatar_hora)  # Formatação automática de hora
+    hora_entry.bind("<KeyRelease>", formatar_hora) 
 
-    # Botão para confirmar agendamento
     global btn_agendar
     btn_agendar = ctk.CTkButton(janela_agendar, text="Confirmar Agendamento", command=confirmar_agendamento)
-    btn_agendar.pack(pady=10)
+    btn_agendar.pack(padx=50, pady=10, fill='x')
 
-    # Botão para voltar para a janela principal
     btn_voltar = ctk.CTkButton(janela_agendar, text="Voltar", command=janela_agendar.destroy)
-    btn_voltar.pack(pady=5)
+    btn_voltar.pack(pady=15)
 
 def formatar_data(event):
     text = data_entry.get()
     if len(text) == 2 or len(text) == 5:
-        data_entry.insert(len(text), "/")  # Adiciona "/" após dia e mês
+        data_entry.insert(len(text), "/")
 
 def formatar_hora(event):
     text = hora_entry.get()
     if len(text) == 2:
-        hora_entry.insert(len(text), ":")  # Adiciona ":" após horas
-
+        hora_entry.insert(len(text), ":")
 def confirmar_agendamento():
     global captura_ativa, timer_thread
     captura_ativa = True
-    escolher_pasta_salvar()  # Escolher a pasta antes de agendar a captura
+    escolher_pasta_salvar()  
     if not pasta_salvar:
-        return  # Cancela a operação se o usuário não escolher a pasta
+        return  
 
     data_str = data_entry.get()
     hora_str = hora_entry.get()
 
     try:
-        # Combinar data e hora em um único objeto datetime
         data_hora_str = f"{data_str} {hora_str}"
         data_hora = datetime.strptime(data_hora_str, "%d/%m/%Y %H:%M")
 
-        # Calcular a diferença até o horário agendado
         agora = datetime.now()
         if data_hora <= agora:
             messagebox.showerror("Erro", "A data e hora devem ser futuras.")
             return
 
-        # Calcular o tempo até o agendamento
         tempo_ate_agendamento = (data_hora - agora).total_seconds()
 
-        # Desabilitar botões e entradas durante o agendamento
-        btn_agendar.configure(state='disabled')  # Desabilitar o botão após confirmar
+        btn_agendar.configure(state='disabled')  
         data_entry.configure(state='disabled')
         hora_entry.configure(state='disabled')
 
-        # Iniciar o timer na tela de agendamento
         start_timer(tempo_ate_agendamento)
 
         threading.Timer(tempo_ate_agendamento, iniciar_captura_agendada).start()
@@ -289,17 +270,40 @@ def confirmar_agendamento():
         messagebox.showerror("Erro", "Formato de data ou hora inválido.")
 
 def start_timer(tempo):
+    max_time_limit = 365 * 24 * 60 * 60  
+
+    if tempo > max_time_limit:
+        messagebox.showerror("Erro", "O tempo máximo permitido é de 1 ano.")
+        return  
+
     global temporizador_label, timer_thread
-    temporizador_label = ctk.CTkLabel(janela_agendar, text="", font=("Arial", 16))
+    temporizador_label = ctk.CTkLabel(janela_agendar, text="", font=("Consolas", 16))
     temporizador_label.pack(pady=20)
 
     def update_timer():
         nonlocal tempo
         while tempo > 0:
-            mins, secs = divmod(int(tempo), 60)
-            temporizador_label.configure(text=f"Tempo até a captura: {mins:02}:{secs:02}")
-            time.sleep(1)
-            tempo -= 1
+            if tempo >= 2592000:  
+                meses = tempo // 2592000  
+                temporizador_label.configure(text=f"Tempo até a captura: {meses} mês(es)")
+                time.sleep(3600)  
+            elif tempo >= 86400:  
+                dias = tempo // 86400
+                temporizador_label.configure(text=f"Tempo até a captura: {dias} dia(s)")
+                time.sleep(3600)  
+            elif tempo >= 3600:  
+                horas, mins = divmod(tempo, 3600)
+                temporizador_label.configure(text=f"Tempo até a captura: {horas} hora(s) {mins // 60} minuto(s)")
+                time.sleep(60)  
+            elif tempo >= 60:  
+                mins, secs = divmod(tempo, 60)
+                temporizador_label.configure(text=f"Tempo até a captura: {mins} minuto(s) {secs} segundo(s)")
+                time.sleep(1) 
+            else:
+                temporizador_label.configure(text=f"Tempo até a captura: {tempo} segundo(s)")
+                time.sleep(1)  
+            
+            tempo -= 1  
 
         temporizador_label.configure(text="Captura em Andamento...")
         finalizar_agendamento()
@@ -307,11 +311,12 @@ def start_timer(tempo):
     timer_thread = threading.Thread(target=update_timer)
     timer_thread.start()
 
+
+
 def finalizar_agendamento():
     global captura_ativa
     captura_ativa = False
 
-    # Habilitar botões e entradas após a captura
     btn_agendar.configure(state='normal')
     data_entry.configure(state='normal')
     hora_entry.configure(state='normal')
@@ -319,19 +324,18 @@ def finalizar_agendamento():
 def iniciar_captura_agendada():
     global captura_ativa
     if captura_ativa:
-        for _ in range(5):  # Tirar 5 fotos
+        for _ in range(7):  
             image_data = tirar_foto()
             if image_data:
                 image = Image.open(io.BytesIO(image_data))
                 salvar_automatico(image)
-            time.sleep(2)  # Intervalo de 2 segundos entre as fotos
+            time.sleep(4) 
 
-# Janela principal
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
-app.geometry("500x650")
+app.geometry("500x700")
 app.title("SnapLink")
 
 tentar_conectar_novamente()
@@ -339,15 +343,12 @@ tentar_conectar_novamente()
 label_titulo = ctk.CTkLabel(app, text="Snap Link", font=("Consolas bold", 24))
 label_titulo.pack(padx=10, pady=10)
 
-# Botão para abrir a janela de captura com temporizador
 btn_temporizador = ctk.CTkButton(app, text="Captura com Temporizador", command=abrir_janela_temporizador)
 btn_temporizador.pack(padx=50, pady=10, fill='x')
 
-# Botão para agendar captura
 btn_agendar = ctk.CTkButton(app, text="Agendar Captura", command=abrir_janela_agendar_captura)
 btn_agendar.pack(padx=50, pady=10, fill='x')
 
-# Botão para capturar uma foto única
 btn_capturar = ctk.CTkButton(app, text="Capturar Imagem", command=tirar_foto_unica)
 btn_capturar.pack(padx=50, pady=10, fill='x')
 
